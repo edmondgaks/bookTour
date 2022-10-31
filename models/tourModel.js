039c88d72f3231f3d9a967a7ad65425de075f464
Edmond GAKS <edmondgaks@gmail.com>
2022-10-31 19:16:45 +0200

    adding other models

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,6 +6,18 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must have a name'],
         unique: true
     },
+    durations: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
     rating: {
         type: Number,
         default: 4.5
