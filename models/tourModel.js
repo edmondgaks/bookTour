7a09570dbb98f145b8a7307c5d16df431ef88002
Edmond GAKS <edmondgaks@gmail.com>
2022-10-31 20:28:29 +0200

    finishing the tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,9 +4,10 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    durations: {
+    duration: {
         type: Number,
         required: [true, 'A tour must have a duration']
     },
@@ -16,16 +17,45 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: Number,
-        required: [true, 'A tour must have a group size']
+        required: [true, 'A tour must have a difficulty']
     },
     rating: {
         type: Number,
         default: 4.5
     },
+    ratingsAverage: {
+        type: Number,
+        default: 4.5
+    },
+    ratingsQuantity: {
+        type: Number, 
+        default: 0
+    },
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description']
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images: String,
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
+    
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
